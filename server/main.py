78abf21b6586fe02e2fd74e9a53ae85cdb70b6bc
lazyc97 <lazycoder97@gmail.com>
2017-09-06 12:04:10 +0700

    add server auto-reload

--- server/main.py
+++ server/main.py
@@ -1,7 +1,14 @@
 from flask import Flask, render_template
+import sys
 
 app = Flask(__name__, template_folder='../client/dist', static_folder='../client/dist/static')
 app.url_map.strict_slashes = False
+
+try:
+    sys.argv.index("production")
+    app.debug = False
+except:
+    app.debug = True
 
 @app.route('/')
 def index():
