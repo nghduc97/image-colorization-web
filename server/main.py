7c81b88e989f4bf09c84baa1a50e0d5d8d1f0447
lazyc97 <lazycoder97@gmail.com>
2017-09-07 08:44:13 +0700

    add network + remove some unused params

--- server/main.py
+++ server/main.py
@@ -1,12 +1,14 @@
 from flask import Flask, send_file
 import sys
 
+# init
 app = Flask(__name__, static_folder='../template/static')
 app.url_map.strict_slashes = False
 
+# index route
 @app.route('/', methods=["GET"])
 def index():
     return send_file("../template/index.html")
 
 if __name__ == '__main__':
-    app.run(port=5000, debug=True)
+    app.run()
