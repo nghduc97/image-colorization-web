55acde3b00511f9b4b180ee728d219db3ecef3bd
lazyc97 <lazycoder97@gmail.com>
2017-09-16 19:33:40 +0700

    split App.vue template into parts

---
+++ client/src/components/Sidebar.vue
@@ -0,0 +1,46 @@
+<template>
+  <md-sidenav class="md-left" ref="sidebar">
+    <md-toolbar class="md-intermediate">
+      <div class="md-toolbar-container">
+        <h3 class="md-title">Navigation</h3>
+      </div>
+    </md-toolbar>
+    <md-list>
+      <md-list-item>
+        <md-ink-ripple />
+        <md-icon>home</md-icon>
+        <span>Home</span>
+      </md-list-item>
+      <md-list-item>
+        <md-ink-ripple />
+        <md-icon>explore</md-icon>
+        <span>Explore</span>
+      </md-list-item>
+      <md-divider class="md-dense"></md-divider>
+      <md-list-item>
+        <md-ink-ripple />
+        <md-icon>cloud_upload</md-icon>
+        <span>Your Uploads</span>
+      </md-list-item>
+      <md-list-item>
+        <md-ink-ripple />
+        <md-icon>forum</md-icon>
+        <span>Your Comments</span>
+      </md-list-item>
+    </md-list>
+  </md-sidenav>
+</template>
+
+<script>
+export default {
+  name: 'sidebar',
+  methods: {
+    toggle () {
+      this.$refs.sidebar.toggle()
+    }
+  }
+}
+</script>
+
+<style>
+</style>
---
+++ client/src/components/Toolbar.vue
@@ -0,0 +1,31 @@
+<template>
+  <md-toolbar>
+    <md-button class="md-icon-button" @click="toggleSideBar">
+      <md-icon>menu</md-icon>
+    </md-button>
+
+    <h1 class="md-title" style="flex: 1">{{ pageName }}</h1>
+
+    <md-button v-if="auth">Logout</md-button>
+    <md-button v-else>Login</md-button>
+  </md-toolbar>
+</template>
+
+<script>
+export default {
+  props: ['auth', 'sidebarRef'],
+  methods: {
+    toggleSideBar () {
+      this.sidebarRef.toggle()
+    }
+  },
+  computed: {
+    pageName () {
+      return this.$store.state.navModule['currentPage']
+    }
+  }
+}
+</script>
+
+<style>
+</style>
