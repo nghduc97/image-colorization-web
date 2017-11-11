9ab85d14b0f671a27c4c5970d669aac43ff96b0f
Duc Hoang Nguyen <lazycoder97@gmail.com>
2017-11-11 20:31:09 +0700

    remove un-used NavModule

--- client/src/components/Dashboard.vue
+++ client/src/components/Dashboard.vue
@@ -8,9 +8,6 @@ import { mapState } from 'vuex'
 
 export default {
   name: 'dashboard',
-  mounted () {
-    this.$store.commit('nav/changePage', 'Dashboard')
-  },
   computed: {
     ...mapState('auth', ['userInfo'])
   }
