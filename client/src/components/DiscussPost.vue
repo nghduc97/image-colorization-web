8d453229e6bfa80daead2941a01d7eb9b65e017f
Duc Hoang Nguyen <lazycoder97@gmail.com>
2017-11-25 20:20:44 +0700

    fix warning

--- client/src/components/DiscussPost.vue
+++ client/src/components/DiscussPost.vue
@@ -13,7 +13,7 @@
             <span>{{ post['total_claps'] + pendingClap }}</span>
             <b-icon icon="sign-language" size="is-small"></b-icon>
           </a>
-          <a class="button is-primary is-inverted" @click="showComments">
+          <a class="button is-primary is-inverted">
             <span>Comments</span>
             <b-icon icon="comments" size="is-small"></b-icon>
           </a>
